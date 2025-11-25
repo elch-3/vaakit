374e299a42d4a5e65f117bf66fb8c64caf2c29cb
oae <oae@saunalahti.fi>
2025-11-25 19:55:14 +0200

    Initial Commit

---
+++ VaaKit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VaaKit
+//
+//  Created by Abc Abc on 25.11.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VaaKitTests/VaaKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaaKitTests.swift
+//  VaaKitTests
+//
+//  Created by Abc Abc on 25.11.2025.
+//
+
+import Testing
+@testable import VaaKit
+
+struct VaaKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
